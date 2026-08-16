729bc66ab3579a79c14bf5738651ea5640780536
Vedant Shende <btech10662.24@bitmesra.ac.in>
2026-08-16 18:52:02 +0530

    LC-number-of-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

---
+++ longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int> arr) {
+        int n = arr.size();
+        vector<int> dp(n, 1);
+        int lis = 0;
+        vector<int> cnt(n, 1);
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(arr[i]>arr[j]){
+                    if(dp[i]<dp[j]+1){
+                        dp[i] = dp[j]+1;
+                        cnt[i] = cnt[j];
+                    }
+                    else if(dp[i] == dp[j]+1){
+                        cnt[i]+=cnt[j];
+                    }
+                }
+                lis = max(lis, dp[i]);
+            }
+        }
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            if(dp[i] == lis)
+                ans += cnt[i];
+        }
+
+        return ans;
+    }
+};
